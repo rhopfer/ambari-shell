fd959726945aadffa1b17f9e667395299b6adba0
lalyos <lalyos@yhoo.com>
2014-04-07 00:58:14 +0200

    cleanup system.out print

--- src/main/java/com/sequenceiq/ambari/shell/AmbariShell.java
+++ src/main/java/com/sequenceiq/ambari/shell/AmbariShell.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.shell.commands.ExitCommands;
-import org.springframework.shell.core.CommandMarker;
-import org.springframework.shell.core.Converter;
 import org.springframework.shell.core.ExitShellRequest;
 import org.springframework.shell.core.JLineShellComponent;
 
@@ -20,25 +18,11 @@ public class AmbariShell implements CommandLineRunner
 	@Autowired
 	GenericApplicationContext ctx;
 
-	public void run(String... arg0) throws Exception {
-		
-		System.out.println("ambari shell ..");
-		for (String cmd : ctx.getBeanNamesForType(CommandMarker.class)) {
-			System.out.format("cmd: %20s %n", cmd);
-		}
-
-		for (String cmd : ctx.getBeanNamesForType(Converter.class)) {
-			System.out.format("converter: %20s %n", cmd);
-		}
-
+	public void run(String... arg0) throws Exception {		
 		JLineShellComponent shell = ctx.getBean("shell", JLineShellComponent.class);
 		ExitShellRequest exitShellRequest;
 
-		try {
 		shell.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		shell.promptLoop();
 		exitShellRequest = shell.getExitShellRequest();
 		if (exitShellRequest == null) {
@@ -48,17 +32,11 @@ public class AmbariShell implements CommandLineRunner
 		shell.waitForComplete();
 
 		ctx.close();
-		
 	}
 	
 	public static void main(String[] args) {
-		try {
 		new SpringApplicationBuilder(AmbariShell.class)
-		//.sources(ShellConfiguration.class)
 		.showBanner(false)
 		.run(args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
