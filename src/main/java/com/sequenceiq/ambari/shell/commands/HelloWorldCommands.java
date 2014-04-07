894e6304038c129d3cf299799c329a71f3ad9717
lalyos <lalyos@yhoo.com>
2014-04-07 14:23:32 +0200

    hello command to print banner.txt

--- src/main/java/com/sequenceiq/ambari/shell/commands/HelloWorldCommands.java
+++ src/main/java/com/sequenceiq/ambari/shell/commands/HelloWorldCommands.java
@@ -3,8 +3,10 @@ package com.sequenceiq.ambari.shell.commands;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
 import org.springframework.shell.core.annotation.CliCommand;
-import org.springframework.shell.core.annotation.CliOption;
+import org.springframework.shell.support.util.FileUtils;
 import org.springframework.stereotype.Component;
+
+import com.sequenceiq.ambari.shell.AmbariShell;
 
 @Component
 public class HelloWorldCommands implements CommandMarker {
@@ -14,13 +16,8 @@ public class HelloWorldCommands implements CommandMarker {
     return true;
   }
 
-  @CliCommand(value = "hw simple", help = "Print a simple hello world message")
-  public String simple(
-    @CliOption(key = { "message" }, mandatory = true, help = "The hello world message") final String message,
-    @CliOption(key = { "location" }, mandatory = false, help = "Where you are saying hello", specifiedDefaultValue="At work") 
-                 final String location) {
-
-    return "Message = [" + message + "] Location = [" + location + "]";
-
+  @CliCommand(value = "hello", help = "Print a simple hello world message")
+  public String simple() {
+	  return FileUtils.readBanner(AmbariShell.class, "banner.txt");
   }
 }
