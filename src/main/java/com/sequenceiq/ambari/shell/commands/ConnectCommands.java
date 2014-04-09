c7479e83b2a500709ee3fcdf546644ac58b1933d
lalyos <lalyos@yhoo.com>
2014-04-09 21:33:22 +0200

    implemented commands: connect/hosts/services/tasks

---
+++ src/main/java/com/sequenceiq/ambari/shell/commands/ConnectCommands.java
@@ -0,0 +1,74 @@
+package com.sequenceiq.ambari.shell.commands;
+
+import org.springframework.shell.core.CommandMarker;
+import org.springframework.shell.core.annotation.CliAvailabilityIndicator;
+import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
+import org.springframework.stereotype.Component;
+
+import com.sequenceiq.ambari.client.AmbariClient;
+
+@Component
+public class ConnectCommands implements CommandMarker {
+	
+	private AmbariClient client = null;
+	
+	@CliAvailabilityIndicator({"connect"})
+	public boolean isConnectCommandAvailable() {
+	    return true;
+	}
+	
+	@CliCommand(value = "connect", help = "Connects to an Ambari Server")
+	  public String conect(
+			  @CliOption(key = { "host" }, mandatory = false, help = "Hostname of Ambari Server; default is: 'localhost'", unspecifiedDefaultValue="localhost")
+			  final String host,
+			  @CliOption(key = { "port" }, mandatory = false, help = "Port number Ambari listens on; default is: '8080'", unspecifiedDefaultValue="8080")
+			  final String port,
+			  @CliOption(key = { "user" }, mandatory = false, help = "Username for authorization; default is: 'admin'", unspecifiedDefaultValue="admin")
+			  final String user,
+			  @CliOption(key = { "password" }, mandatory = false, help = "Password of the user; default is: 'admin'", unspecifiedDefaultValue="admin")
+			  final String password ) {
+		  
+		try {
+			client = new AmbariClient(host, port, user, password);
+			return client.clusterList();
+		} catch (Exception e){
+			return "connection failure: " + e.getMessage();
+		}
+	  }
+	
+	
+	@CliAvailabilityIndicator({"tasks"})
+	public boolean isTasksCommandAvailable() {
+	    return client != null;
+	}
+	
+	@CliCommand(value = "tasks", help = "Connects to an Ambari Server")
+	  public String tasks(
+			  @CliOption(key = { "id" }, mandatory = false, help = "id of the Task; default is: 1", unspecifiedDefaultValue="1")
+			  String id
+	   ){
+		return client.taskList(id);
+	}
+
+	@CliAvailabilityIndicator({"hosts"})
+	public boolean isHostsCommandAvailable() {
+	    return client != null;
+	}
+	
+	@CliCommand(value = "hosts", help = "lists available hosts")
+	  public String hosts(){
+		return client.hostList();
+	}
+	
+	@CliAvailabilityIndicator({"services"})
+	public boolean isServicesCommandAvailable() {
+	    return client != null;
+	}
+	
+	@CliCommand(value = "services", help = "lists available services")
+	  public String services(){
+		return client.serviceList();
+	}
+
+}
