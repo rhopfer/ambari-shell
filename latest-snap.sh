e01435d0784e2d5dc4cf1b21b6de3a408332901f
lalyos <lalyos@yhoo.com>
2014-04-10 18:45:44 +0200

    package and artefact name standardized

--- latest-snap.sh
+++ latest-snap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-SNAPSHOT_URL=http://seq-repo.s3-website-eu-west-1.amazonaws.com/snapshots
-PACKAGE=com/sequenceiq
+SNAPSHOT_URL=http://maven.sequenceiq.com/snapshots
+PACKAGE=com/sequenceiq/ambari
 ARTIFACT=ambari-shell
 FULLNAME=$PACKAGE/$ARTIFACT
 
-VERSION=$(curl -s $SNAPSHOT_URL/$FULLNAME/maven-metadata.xml|sed -n "s/.*<version>\([^<]*\).*/\1/p")
+VERSION=$(curl -Ls $SNAPSHOT_URL/$FULLNAME/maven-metadata.xml|sed -n "s/.*<version>\([^<]*\).*/\1/p")
 
-LATEST=$(curl -s $SNAPSHOT_URL/$FULLNAME/$VERSION/maven-metadata.xml|sed -n "/>jar</ {n;s/.*<value>\([^<]*\).*/\1/p;}"|tail -1)
+LATEST=$(curl -Ls $SNAPSHOT_URL/$FULLNAME/$VERSION/maven-metadata.xml|sed -n "/>jar</ {n;s/.*<value>\([^<]*\).*/\1/p;}"|tail -1)
 
 curl -so shell.jar $SNAPSHOT_URL/$FULLNAME/$VERSION/$ARTIFACT-$LATEST.jar
 java -jar shell.jar
