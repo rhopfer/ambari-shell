1a2f88ce0c2723aebfabc11a5dc596ba22601963
lalyos <lalyos@yhoo.com>
2014-04-14 18:25:16 +0200

    only selected default commands are used: version / exit / help

--- src/main/java/com/sequenceiq/ambari/shell/AmbariShell.java
+++ src/main/java/com/sequenceiq/ambari/shell/AmbariShell.java
@@ -3,17 +3,37 @@ package com.sequenceiq.ambari.shell;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.shell.commands.ExitCommands;
+import org.springframework.shell.commands.HelpCommands;
+import org.springframework.shell.commands.VersionCommands;
+import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.ExitShellRequest;
 import org.springframework.shell.core.JLineShellComponent;
 
 @Configuration
-@ComponentScan(basePackageClasses={AmbariShell.class,ExitCommands.class})
+@ComponentScan(basePackageClasses={AmbariShell.class})
 public class AmbariShell implements CommandLineRunner 
 {
+	
+	@Bean
+	public CommandMarker exitCommand() {
+		return new ExitCommands();
+	}
+
+	@Bean
+	public CommandMarker versionCommands() {
+		return new VersionCommands();
+	}
+
+	@Bean
+	public CommandMarker helpCommands() {
+		return new HelpCommands();
+	}
+
 	
 	@Autowired
 	GenericApplicationContext ctx;
