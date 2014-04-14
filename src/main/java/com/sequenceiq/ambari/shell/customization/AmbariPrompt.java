a9fb64bf9b9a3ae93b89cacf8d396e421942bbf6
lalyos <lalyos@yhoo.com>
2014-04-14 18:23:55 +0200

    dynamic prompt generation moved to AmbariContext

--- src/main/java/com/sequenceiq/ambari/shell/customization/AmbariPrompt.java
+++ src/main/java/com/sequenceiq/ambari/shell/customization/AmbariPrompt.java
@@ -1,17 +1,23 @@
 package com.sequenceiq.ambari.shell.customization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.plugin.PromptProvider;
 import org.springframework.stereotype.Component;
+
+import com.sequenceiq.ambari.shell.commands.AmbariContext;
 
 @Component
 public class AmbariPrompt implements PromptProvider{
 
+	@Autowired
+	AmbariContext context;
+	
 	public String getProviderName() {
 		return AmbariPrompt.class.getSimpleName();
 	}
 
 	public String getPrompt() {
-		return "ambari-shell>";
+		return context.getPrompt();
 	}
 
 }
