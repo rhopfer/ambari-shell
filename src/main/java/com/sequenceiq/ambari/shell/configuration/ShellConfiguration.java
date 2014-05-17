72a373dd79969bb05a101380ea5a7347d0d69d87
keyki <keyki.kk@gmail.com>
2014-05-17 09:41:55 +0200

    separated configurations

--- src/main/java/com/sequenceiq/ambari/shell/configuration/ShellConfiguration.java
+++ src/main/java/com/sequenceiq/ambari/shell/configuration/ShellConfiguration.java
@@ -27,24 +27,7 @@ import org.springframework.shell.commands.ExitCommands;
 import org.springframework.shell.commands.HelpCommands;
 import org.springframework.shell.commands.ScriptCommands;
 import org.springframework.shell.commands.VersionCommands;
-import org.springframework.shell.converters.AvailableCommandsConverter;
-import org.springframework.shell.converters.BigDecimalConverter;
-import org.springframework.shell.converters.BigIntegerConverter;
-import org.springframework.shell.converters.BooleanConverter;
-import org.springframework.shell.converters.CharacterConverter;
-import org.springframework.shell.converters.DateConverter;
-import org.springframework.shell.converters.DoubleConverter;
-import org.springframework.shell.converters.EnumConverter;
-import org.springframework.shell.converters.FloatConverter;
-import org.springframework.shell.converters.IntegerConverter;
-import org.springframework.shell.converters.LocaleConverter;
-import org.springframework.shell.converters.LongConverter;
-import org.springframework.shell.converters.ShortConverter;
-import org.springframework.shell.converters.SimpleFileConverter;
-import org.springframework.shell.converters.StaticFieldConverterImpl;
-import org.springframework.shell.converters.StringConverter;
 import org.springframework.shell.core.CommandMarker;
-import org.springframework.shell.core.Converter;
 import org.springframework.shell.core.JLineShellComponent;
 import org.springframework.shell.plugin.HistoryFileNameProvider;
 import org.springframework.shell.plugin.support.DefaultHistoryFileNameProvider;
@@ -70,18 +53,13 @@ public class ShellConfiguration {
   private String password;
 
   @Bean
-  public AmbariClient createAmbariClient() {
+  AmbariClient createAmbariClient() {
     return new AmbariClient(host, port, user, password);
   }
 
   @Bean
-  public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+  static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
     return new PropertySourcesPlaceholderConfigurer();
-  }
-
-  @Bean
-  public ScriptCommands scriptCommands(){
-    return new ScriptCommands();
   }
 
   @Bean
@@ -100,86 +78,6 @@ public class ShellConfiguration {
   }
 
   @Bean
-  Converter SimpleFileConverter() {
-    return new SimpleFileConverter();
-  }
-
-  @Bean
-  Converter stringConverter() {
-    return new StringConverter();
-  }
-
-  @Bean
-  Converter availableCommandsConverter() {
-    return new AvailableCommandsConverter();
-  }
-
-  @Bean
-  Converter bigDecimalConverter() {
-    return new BigDecimalConverter();
-  }
-
-  @Bean
-  Converter bigIntegerConverter() {
-    return new BigIntegerConverter();
-  }
-
-  @Bean
-  Converter booleanConverter() {
-    return new BooleanConverter();
-  }
-
-  @Bean
-  Converter characterConverter() {
-    return new CharacterConverter();
-  }
-
-  @Bean
-  Converter dateConverter() {
-    return new DateConverter();
-  }
-
-  @Bean
-  Converter doubleConverter() {
-    return new DoubleConverter();
-  }
-
-  @Bean
-  Converter enumConverter() {
-    return new EnumConverter();
-  }
-
-  @Bean
-  Converter floatConverter() {
-    return new FloatConverter();
-  }
-
-  @Bean
-  Converter integerConverter() {
-    return new IntegerConverter();
-  }
-
-  @Bean
-  Converter LocaleConverter() {
-    return new LocaleConverter();
-  }
-
-  @Bean
-  Converter longConverter() {
-    return new LongConverter();
-  }
-
-  @Bean
-  Converter shortConverter() {
-    return new ShortConverter();
-  }
-
-  @Bean
-  Converter StaticFieldConverterImpl() {
-    return new StaticFieldConverterImpl();
-  }
-
-  @Bean
   CommandMarker exitCommand() {
     return new ExitCommands();
   }
@@ -192,5 +90,10 @@ public class ShellConfiguration {
   @Bean
   CommandMarker helpCommands() {
     return new HelpCommands();
+  }
+
+  @Bean
+  CommandMarker scriptCommands() {
+    return new ScriptCommands();
   }
 }
