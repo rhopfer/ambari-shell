70ec6e485284f34a1f957298752b8aa0120f06ae
keyki <keyki.kk@gmail.com>
2014-05-22 08:49:41 +0200

    javadoc for hints

--- src/main/java/com/sequenceiq/ambari/shell/model/Hints.java
+++ src/main/java/com/sequenceiq/ambari/shell/model/Hints.java
@@ -22,10 +22,29 @@ package com.sequenceiq.ambari.shell.model;
  */
 public enum Hints {
 
+  /**
+   * Hint for adding blueprints.
+   */
   ADD_BLUEPRINT("Add a blueprint with the 'blueprint add' or add the default blueprints with the 'blueprint defaults' command."),
+
+  /**
+   * Hint for start building a cluster.
+   */
   BUILD_CLUSTER("Start building a cluster with the 'cluster build' command using a previously added blueprint."),
+
+  /**
+   * Hint for start assigning hosts to host groups in cluster build phase.
+   */
   ASSIGN_HOSTS("Assign hosts to different host groups with the 'cluster assign' command."),
+
+  /**
+   * Hint for create a cluster from the assigned hosts.
+   */
   CREATE_CLUSTER("Create the cluster with the 'cluster create' command or use the 'cluster reset' command and start over."),
+
+  /**
+   * Hint for check the cluster creation result.
+   */
   PROGRESS("See the install progress with the 'tasks' command.");
 
   private final String message;
