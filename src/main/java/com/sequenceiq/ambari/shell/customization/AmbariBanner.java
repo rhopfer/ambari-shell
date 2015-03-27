4718f7207fb48266d8d91c1df14df2c80859c9de
Richard Doktorics <doktoricsrichard@gmail.com>
2015-03-27 16:30:39 +0100

    CLOUD-666 removed unused dependency

--- src/main/java/com/sequenceiq/ambari/shell/customization/AmbariBanner.java
+++ src/main/java/com/sequenceiq/ambari/shell/customization/AmbariBanner.java
@@ -18,9 +18,10 @@
 package com.sequenceiq.ambari.shell.customization;
 
 import org.springframework.shell.plugin.BannerProvider;
+import org.springframework.shell.support.util.FileUtils;
 import org.springframework.stereotype.Component;
 
-import com.github.lalyos.jfiglet.FigletFont;
+import com.sequenceiq.ambari.shell.AmbariShell;
 
 /**
  * Prints the banner when the user starts the shell.
@@ -29,22 +30,22 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class AmbariBanner implements BannerProvider {
 
   @Override
-  public String getProviderName() {
-    return "AmbariShell";
-  }
+    public String getProviderName() {
+      return "AmbariShell";
+    }
 
-  @Override
-  public String getBanner() {
-    return FigletFont.convertOneLine("AmbariShell");
-  }
+    @Override
+    public String getBanner() {
+      return FileUtils.readBanner(AmbariShell.class, "banner.txt");
+    }
 
-  @Override
-  public String getVersion() {
-    return getClass().getPackage().getImplementationVersion();
-  }
+    @Override
+    public String getVersion() {
+      return getClass().getPackage().getImplementationVersion();
+    }
 
-  @Override
-  public String getWelcomeMessage() {
-    return "Welcome to Ambari Shell. For command and param completion press TAB, for assistance type 'hint'.";
-  }
+    @Override
+    public String getWelcomeMessage() {
+      return "Welcome to Ambari Shell. For command and param completion press TAB, for assistance type 'hint'.";
+    }
 }
